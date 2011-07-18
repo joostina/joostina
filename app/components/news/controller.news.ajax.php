4e282b8104e92965e67d0537dcad3d29ca13ed11
boston <bost56@gmail.com>
2011-07-19 03:25:17 +0600

    Форматирование кода

--- app/components/news/controller.news.ajax.php
+++ app/components/news/controller.news.ajax.php
@@ -10,37 +10,35 @@
 // запрет прямого доступа
 defined('_JOOS_CORE') or die();
 
+class actionsNews extends joosController {
 
-class actionsNews extends joosController
-{
+	public static function index() {
 
-    public static function index()
-    {
+		joosLoader::lib('valumsfileuploader', 'files');
+		joosLoader::lib('images');
 
-        joosLoader::lib('valumsfileuploader', 'files');
-        joosLoader::lib('images');
+		$image_id = joosRequest::request('image_id', 0);
+		$image_id = $image_id ? $image_id : false;
 
-        $image_id = joosRequest::request('image_id', 0);
-        $image_id = $image_id ? $image_id : false;
+		//Загружаем оригинальное изображение (original.png)
+		$file = ValumsfileUploader::upload('original', 'news', $image_id, false);
 
-        //Загружаем оригинальное изображение (original.png)
-        $file = ValumsfileUploader::upload('original', 'news', $image_id, false);
+		//Путь к оригинальному изображению
+		$img = dirname($file['basename']);
 
-        //Путь к оригинальному изображению
-        $img = dirname($file['basename']);
+		//Большая картинка
+		Thumbnail::output($file['basename'], $img . '/big.png', array('width' => 555, 'height' => 555, 'method' => THUMBNAIL_METHOD_SCALE_MIN));
 
-        //Большая картинка
-        Thumbnail::output($file['basename'], $img . '/big.png', array('width' => 555, 'height' => 555, 'method' => THUMBNAIL_METHOD_SCALE_MIN));
+		//Средняя картинка
+		Thumbnail::output($file['basename'], $img . '/medium.png', array('width' => 300, 'height' => 300, 'method' => THUMBNAIL_METHOD_SCALE_MIN));
 
-        //Средняя картинка
-        Thumbnail::output($file['basename'], $img . '/medium.png', array('width' => 300, 'height' => 300, 'method' => THUMBNAIL_METHOD_SCALE_MIN));
+		//Превью
+		//Сначала уменьшаем
+		Thumbnail::output($file['basename'], $img . '/thumb.png', array('width' => 158, 'height' => 108, 'method' => THUMBNAIL_METHOD_SCALE_MIN));
+		//а потом обрезаем
+		Thumbnail::output($img . '/thumb.png', $img . '/thumb.png', array('width' => 158, 'height' => 108, 'method' => THUMBNAIL_METHOD_CROP));
 
-        //Превью
-        //Сначала уменьшаем
-        Thumbnail::output($file['basename'], $img . '/thumb.png', array('width' => 158, 'height' => 108, 'method' => THUMBNAIL_METHOD_SCALE_MIN));
-        //а потом обрезаем
-        Thumbnail::output($img . '/thumb.png', $img . '/thumb.png', array('width' => 158, 'height' => 108, 'method' => THUMBNAIL_METHOD_CROP));
+		echo json_encode(array('location' => $file['location'], 'file_id' => $file['file_id'], 'livename' => $file['livename'], 'success' => true));
+	}
 
-        echo json_encode(array('location' => $file['location'], 'file_id' => $file['file_id'], 'livename' => $file['livename'], 'success' => true));
-    }
 }
