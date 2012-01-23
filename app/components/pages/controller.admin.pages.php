6ad4d50bf144b33eba0c103d6467e11454c61dce
boston <bost56@gmail.com>
2012-01-23 13:08:04 +0500

    fix контроллера страниц

--- app/components/pages/controller.admin.pages.php
+++ app/components/pages/controller.admin.pages.php
@@ -140,4 +140,19 @@ class actionsAdminPages {
 				return joosRoute::redirect('index2.php?option=' . $option . '&model=' . self::$model . '&task=edit&id=' . $obj_data->id, 'Всё ок, редактируем дальше');
 				break;
 
-			case 2: // сохранить и добавить 
+			case 2: // сохранить и добавить новое
+				return joosRoute::redirect( 'index2.php?option=' . $option . '&model=' . self::$model . '&task=create' , 'Всё ок, создаём новое' );
+				break;
+		}
+	}
+
+	/**
+	 * Сохранение отредактированного или созданного объекта
+	 * и перенаправление на главную страницу компонента
+	 *
+	 * @param string $option
+	 */
+	public static function save( $option ) {
+		self::save_this( $option );
+	}
+}
