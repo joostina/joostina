d0eee41ba329dd1dce978302a7d2aa6eaecf74f9
boston <bost56@gmail.com>
2012-03-08 00:15:26 +0600

    Новые расширения

--- app/bootstrap.php
+++ app/bootstrap.php
@@ -65,6 +65,7 @@ switch (JENVIRONMENT) {
 	case 'development':
 		// установка режима отображения ошибок
 		//error_reporting(E_ALL & ~E_DEPRECATED ^ E_STRICT);
+		//error_reporting(-1);
 		error_reporting(E_ALL | E_NOTICE | E_STRICT);
 		ini_set('display_errors', 1);
 		break;
