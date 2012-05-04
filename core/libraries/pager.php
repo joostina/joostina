2cd4d56c76c68edbd395031e95dc7b80eaeeaf2d
boston <bost56@gmail.com>
2012-05-04 17:09:57 +0600

    Рефактоинг пагинатора

--- core/libraries/pager.php
+++ core/libraries/pager.php
@@ -12,7 +12,7 @@
  * @license    MIT License http://www.opensource.org/licenses/mit-license.php
  * Информация об авторах и лицензиях стороннего кода в составе Joostina CMS: docs/copyrights
  *
- * @todo	переписать joosPager на joosRoute и привести к общему виду
+ * @todo    переписать joosPager на joosRoute и привести к общему виду
  *
  * @author     Leng Sheng Hong <darkredz@gmail.com>
  * @link       http://www.doophp.com/
@@ -20,306 +20,221 @@
  * @license    http://www.doophp.com/license
  *
  * */
-class joosPager {
-
-	/**
-	 * CSS class name for the modelPages links
-	 *
-	 * @var string
-	 */
-	public $pagesCss = 'paginate';
-	/**
-	 * CSS class name for the modelPages DropDown menu
-	 *
-	 * @var string
-	 */
-	public $dropDownCss = 'pagerDropDown';
-	/**
-	 * CSS class name for the page sizes menu
-	 *
-	 * @var string
-	 */
-	public $pageSizeCss = 'pageSize';
-	/**
-	 * CSS class name for inactive links
-	 *
-	 * @var string
-	 */
-	public $inactivePrevCss = 'inactivePrev';
-	/**
-	 * CSS class name for inactive links
-	 *
-	 * @var string
-	 */
-	public $inactiveNextCss = 'inactiveNext';
-	/**
-	 * CSS class name for selected current link
-	 *
-	 * @var string
-	 */
-	public $currentCss = 'active';
-	/**
-	 * CSS class name for next link
-	 *
-	 * @var string
-	 */
-	public $nextCss = 'next';
-	/**
-	 * CSS class name for previous link
-	 *
-	 * @var string
-	 */
-	public $prevCss = 'prev';
-	/**
-	 * Contain the list of components to be used in view. (pages, jump menu, page_size, current_page, total_page)
-	 *
-	 * @var array
-	 */
-	public $components;
-	/**
-	 * Items to be displayed per page
-	 *
-	 * @var int
-	 */
-	public $itemPerPage = 10;
-	/**
-	 * The current page number
-	 *
-	 * @var int
-	 */
-	public $currentPage = 1;
-	/**
-	 * Maximum Pager length
-	 *
-	 * @var int
-	 */
-	public $maxLength = 10;
-	/**
-	 * Total items to be split in the pagination
-	 *
-	 * @var int
-	 */
-	public $totalItem;
-	/**
-	 * Total pages in the pagination
-	 *
-	 * @var int
-	 */
-	public $totalPage;
-	/**
-	 * The pages HTML output
-	 *
-	 * @var string
-	 */
-	public $output;
-	/**
-	 * The URL prefix for all the pagination links
-	 *
-	 * @var string
-	 */
-	public $baseUrl;
-	//----- for SQL use -----
-	/**
-	 * Position of the record to begin the pagination LIMIT query
-	 *
-	 * @var string
-	 */
-	public $low;
-	/**
-	 * Position of the record to end the pagination LIMIT query
-	 *
-	 * @var string
-	 */
-	public $high;
-	/**
-	 * To be use with the pagination LIMIT query LIMIT $limit
-	 *
-	 * @var string
-	 */
-	public $limit;
-	public $offset;
-	/**
-	 * @var string
-	 */
-	private $_output;
-
-	/**
-	 * Instanciate the Pager object
-	 *
-	 * @param string $baseURL     Base URL to be appended to the page number
-	 * @param int    $totalItems  Total items to be paginate
-	 * @param int    $itemPerPage Items to be shown in one page.
-	 * @param int    $maxlength   Number of links for the pager navigation
-	 * @param string $prevText    joosText for the Previous button link
-	 * @param string $nextText    joosText for the Next button link
-	 */
-	function __construct( $baseURL = '' , $totalItems = 120 , $itemPerPage = 10 , $maxlength = 11 , $extraURL = '' ) {
-		$this->baseUrl     = $baseURL;
-		$this->baseUrl     = $extraURL ? $this->baseUrl . $extraURL : $this->baseUrl;
-		$this->totalItem   = $totalItems;
-		$this->maxLength   = $maxlength;
-		$this->itemPerPage = $itemPerPage;
-	}
-
-	/**
-	 * Paginate the list of items and prepare pager components to be use in View.
-	 *
-	 * @param int $page        The current page number
-	 * @param int $itemPerPage Items per page
-	 *
-	 * @return array An array of pager component, access via <strong>pages, jump_menu, page_size, current_page, total_page, next_link, prev_link</strong>
-	 */
-	public function paginate( $page , $itemPerPage = 0 ) {
-
-		if ( $itemPerPage === 0 ) {
-			$itemPerPage = $this->itemPerPage;
-		} else {
-			$this->itemPerPage = $itemPerPage;
-		}
-
-		$this->totalPage   = ceil( $this->totalItem / $itemPerPage );
-
-		$this->currentPage = (int) $page;
-
-		if ( $this->currentPage < 1 || !is_numeric( $this->currentPage ) ) {
-			$this->currentPage = 1;
-		}
-
-		if ( $this->currentPage > $this->totalPage ) {
-			$this->currentPage = $this->totalPage;
-		}
-
-		$prev_page = $this->currentPage - 1;
-		$next_page = $this->currentPage + 1;
-
-		$this->_output .= '';
-
-		//-----------------------------------------------------------------------------------------------"НАЗАД"
-		$_prev   = "<a class=\"page_left\" href=\"{$this->baseUrl}/page/{$prev_page}\">&larr;</a>";
-        $_class = ( $this->currentPage != 1 ) ? '' : ' class="disabled"';
-		$this->components['prev'] = '<li'.$_class.'>' . $_prev . '</li>';
+class joosPager{
 
 
-		//Вывод джампером (с разрывом, если кол-во страниц > допустимого интервала)
-		if ( $this->totalPage > $this->maxLength ) {
-			$midRange    = $this->maxLength - 2;
+    /**
+     * Contain the list of components to be used in view. (pages, jump menu, page_size, current_page, total_page)
+     *
+     * @var array
+     */
+    public $components;
+    /**
+     * Items to be displayed per page
+     *
+     * @var int
+     */
+    public $item_per_page = 10;
+    /**
+     * The current page number
+     *
+     * @var int
+     */
+    public $current_page = 1;
+    /**
+     * Maximum Pager length
+     *
+     * @var int
+     */
+    public $max_length = 10;
+    /**
+     * Total items to be split in the pagination
+     *
+     * @var int
+     */
+    public $total_item;
+    /**
+     * Total pages in the pagination
+     *
+     * @var int
+     */
+    public $total_page;
+    /**
+     * The pages HTML output
+     *
+     * @var string
+     */
+    public $output;
+    /**
+     * The URL prefix for all the pagination links
+     *
+     * @var string
+     */
+    public $base_url;
+    //----- for SQL use -----
+    /**
+     * Position of the record to begin the pagination LIMIT query
+     *
+     * @var string
+     */
+    public $low;
+    /**
+     * Position of the record to end the pagination LIMIT query
+     *
+     * @var string
+     */
+    public $high;
+    /**
+     * To be use with the pagination LIMIT query LIMIT $limit
+     *
+     * @var string
+     */
+    public $limit;
+    public $offset;
+    /**
+     * @var string
+     */
+    private $_output;
 
-			$start_range = $this->currentPage - floor( $midRange / 2 );
-			$end_range   = $this->currentPage + floor( $midRange / 2 );
+    function __construct($base_url = '', $total_items = 120, $item_per_page = 10, $max_length = 11, $extra_url = ''){
+        $this->base_url = $base_url;
+        $this->base_url = $extra_url ? $this->base_url . $extra_url : $this->base_url;
+        $this->total_item = $total_items;
+        $this->max_length = $max_length;
+        $this->item_per_page = $item_per_page;
+    }
 
-			if ( $start_range <= 0 ) {
-				$end_range += abs( $start_range ) + 1;
-				$start_range = 1;
-			}
+    public function paginate($page, $item_per_page = 0){
 
-			if ( $end_range > $this->totalPage ) {
-				$start_range -= $end_range - $this->totalPage;
-				$end_range = $this->totalPage;
-			}
+        if ($item_per_page === 0) {
+            $item_per_page = $this->item_per_page;
+        } else {
+            $this->item_per_page = $item_per_page;
+        }
 
-			while ( $end_range - $start_range + 1 < $this->maxLength - 1 ) {
-				$end_range++;
-			}
+        $this->total_page = ceil($this->total_item / $item_per_page);
 
-			$modulus = (int) $this->maxLength % 2 == 0;
-			$center  = floor( $this->maxLength / 2 );
+        $this->current_page = (int)$page;
 
-			if ( $this->currentPage > $center ) {
-				$end_range--;
-			}
+        if ($this->current_page < 1 || !is_numeric($this->current_page)) {
+            $this->current_page = 1;
+        }
 
-			if ( $modulus == 0 && $this->totalPage - $this->currentPage + 1 <= $center ) {
-				while ( $end_range - $start_range + 1 < $this->maxLength - 1 ) {
-					$start_range--;
-				}
-			}
-			$range   = range( $start_range , $end_range );
+        if ($this->current_page > $this->total_page) {
+            $this->current_page = $this->total_page;
+        }
 
-			for ( $i = 1; $i <= $this->totalPage; $i++ ) {
-				// loop through all pages. if first, last, or in range, display
-				if ( $i == 1 || $i == $this->totalPage || in_array( $i , $range ) ) {
-					$lastDot = '';
+        $prev_page = $this->current_page - 1;
+        $next_page = $this->current_page + 1;
 
-					if ( $modulus == 1 ) {
-						if ( $i == $this->totalPage && $this->currentPage < ( $this->totalPage - ( $this->maxLength - $center - $modulus ) ) ) {
-							$lastDot = '...';
-						}
-					} else {
-						if ( $i == $this->totalPage && $this->currentPage <= ( $this->totalPage - ( $this->maxLength - $center ) ) ) {
-							$lastDot = '...';
-						}
-					}
+        $this->_output .= '';
 
-					//-----------------------------------------------------------------------------НОМЕРА СТРАНИЦ
-					$_number = "<a href=\"{$this->baseUrl}/page/$i\">$lastDot $i";
+        //-----------------------------------------------------------------------------------------------"НАЗАД"
+        $_prev = "<a class=\"page_left\" href=\"{$this->base_url}/page/{$prev_page}\">&larr;</a>";
+        $_class = ($this->current_page != 1) ? '' : ' class="disabled"';
+        $this->components['prev'] = '<li' . $_class . '>' . $_prev . '</li>';
 
+        //Вывод джампером (с разрывом, если кол-во страниц > допустимого интервала)
+        if ($this->total_page > $this->max_length) {
+            $midRange = $this->max_length - 2;
 
-					if ( $range[0] > 2 && $i == 1 ) {
-						$_number .= " ...</a>";
-					} else {
-						$_number .= '</a>';
-					}
+            $start_range = $this->current_page - floor($midRange / 2);
+            $end_range = $this->current_page + floor($midRange / 2);
 
-                    $_class = ( $i == $this->currentPage ) ? ' class="active"' : '';
-                    $this->_output .= '<li'.$_class.'>'.$_number .'</li>';
-				}
-			}
+            if ($start_range <= 0) {
+                $end_range += abs($start_range) + 1;
+                $start_range = 1;
+            }
 
+            if ($end_range > $this->total_page) {
+                $start_range -= $end_range - $this->total_page;
+                $end_range = $this->total_page;
+            }
 
-			//-----------------------------------------------------------------------------"ВПЕРЕД"
-			$_next  = "<a class=\"page_right\"  href=\"{$this->baseUrl}/page/$next_page\">&rarr;</a>";
-            $_class = ( $this->currentPage != $this->totalPage && $this->totalItem >= $this->maxLength ) ? '' : ' class="disabled"';
-            $this->components['next'] = '<li'.$_class.'>' . $_next . '</li>';
+            while ($end_range - $start_range + 1 < $this->max_length - 1) {
+                $end_range++;
+            }
 
-		} else {
-			//-----------------------------------------------------------------------------НОМЕРА СТРАНИЦ
-			for ( $i = 1; $i <= $this->totalPage; $i++ ) {
-				$_number = "<a href=\"{$this->baseUrl}/page/$i\">$i</a>";
-                $_class = ( $i == $this->currentPage ) ? ' class="active"' : '';
-                $this->_output .= '<li'.$_class.'>'.$_number .'</li>';
-			}
+            $modulus = (int)$this->max_length % 2 == 0;
+            $center = floor($this->max_length / 2);
 
-			//-----------------------------------------------------------------------------"ВПЕРЕД"
-			$_next  = "<a class=\"page_right\"  href=\"{$this->baseUrl}/page/$next_page\">&rarr;</a>";
+            if ($this->current_page > $center) {
+                $end_range--;
+            }
 
-            $_class = ( $this->currentPage != $this->totalPage ) ? '' : ' class="disabled"';
-            $this->components['next'] = '<li'.$_class.'>' . $_next . '</li>';
+            if ($modulus == 0 && $this->total_page - $this->current_page + 1 <= $center) {
+                while ($end_range - $start_range + 1 < $this->max_length - 1) {
+                    $start_range--;
+                }
+            }
+            $range = range($start_range, $end_range);
 
-		}
+            for ($i = 1; $i <= $this->total_page; $i++) {
+                if ($i == 1 || $i == $this->total_page || in_array($i, $range)) {
+                    $lastDot = '';
 
+                    if ($modulus == 1) {
+                        if ($i == $this->total_page && $this->current_page < ($this->total_page - ($this->max_length - $center - $modulus))) {
+                            $lastDot = '...';
+                        }
+                    } else {
+                        if ($i == $this->total_page && $this->current_page <= ($this->total_page - ($this->max_length - $center))) {
+                            $lastDot = '...';
+                        }
+                    }
 
-		$this->low                        = ( $this->currentPage - 1 ) * $this->itemPerPage;
-		$this->high                       = ( $this->currentPage * $this->itemPerPage ) - 1;
-		$this->low                        = $this->low < 0 ? 0 : $this->low;
-		$this->limit                      = $this->itemPerPage;
-		$this->offset                     = $this->low;
+                    //-----------------------------------------------------------------------------НОМЕРА СТРАНИЦ
+                    $_number = "<a href=\"{$this->base_url}/page/$i\">$lastDot $i";
 
 
-		$this->components['current_page'] = $this->currentPage;
-		$this->components['total_page']   = $this->totalPage;
+                    if ($range[0] > 2 && $i == 1) {
+                        $_number .= " ...</a>";
+                    } else {
+                        $_number .= '</a>';
+                    }
 
-		/*
-		  $this->output = '	<div class="pagination">
-		  <ul class="pagenav_list pagenav_next-prev listreset">
-		  <li class="pagenav_list_item pagenav_prev">'.$this->components['prev'].'</li>
-		  <li class="pagenav_list_item pagenav_next">'.$this->components['next'].'</li>
-		  </ul>
+                    $_class = ($i == $this->current_page) ? ' class="active"' : '';
+                    $this->_output .= '<li' . $_class . '>' . $_number . '</li>';
+                }
+            }
 
-		  <ul class="pagenav_list pagenav_items listreset">
-		  '.$this->_output.'
-		  </ul>
-		  </div>';
-		 */
-		//$this->output = $this->components['prev'].$this->_output.$this->components['next'];
-		$this->output              = '<div class="pagination"><ul>' .
-                $this->components['prev'].$this->_output.$this->components['next'] .
-        '</ul></div>';
+            //-----------------------------------------------------------------------------"ВПЕРЕД"
+            $_next = "<a class=\"page_right\"  href=\"{$this->base_url}/page/$next_page\">&rarr;</a>";
+            $_class = ($this->current_page != $this->total_page && $this->total_item >= $this->max_length) ? '' : ' class="disabled"';
+            $this->components['next'] = '<li' . $_class . '>' . $_next . '</li>';
 
-		$this->output              = $this->totalItem > 0 && $this->totalPage > 1 ? $this->output : '';
+        } else {
+            //-----------------------------------------------------------------------------НОМЕРА СТРАНИЦ
+            for ($i = 1; $i <= $this->total_page; $i++) {
+                $_number = "<a href=\"{$this->base_url}/page/$i\">$i</a>";
+                $_class = ($i == $this->current_page) ? ' class="active"' : '';
+                $this->_output .= '<li' . $_class . '>' . $_number . '</li>';
+            }
 
-		$this->components['pages'] = $this->output;
+            //-----------------------------------------------------------------------------"ВПЕРЕД"
+            $_next = "<a class=\"page_right\"  href=\"{$this->base_url}/page/$next_page\">&rarr;</a>";
 
-		return $this->components;
-	}
+            $_class = ($this->current_page != $this->total_page) ? '' : ' class="disabled"';
+            $this->components['next'] = '<li' . $_class . '>' . $_next . '</li>';
+
+        }
+
+        $this->low = ($this->current_page - 1) * $this->item_per_page;
+        $this->high = ($this->current_page * $this->item_per_page) - 1;
+        $this->low = $this->low < 0 ? 0 : $this->low;
+        $this->limit = $this->item_per_page;
+        $this->offset = $this->low;
+
+        $this->components['current_page'] = $this->current_page;
+        $this->components['total_page'] = $this->total_page;
+
+        $this->output = '<div class="pagination"><ul>' .$this->components['prev'] . $this->_output . $this->components['next'] . '</ul></div>';
+
+        $this->output = $this->total_item > 0 && $this->total_page > 1 ? $this->output : '';
+
+        $this->components['pages'] = $this->output;
+
+        return $this->components;
+    }
 
 }
